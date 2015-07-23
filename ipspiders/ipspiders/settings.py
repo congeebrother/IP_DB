1c21a459d6458eb9ca6190e32aea951880235a0e
congeebrother <daniel_001@126.com>
2015-07-23 11:42:05 +0800

    scrapy init

---
+++ ipspiders/ipspiders/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for ipspiders project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'ipspiders'
+
+SPIDER_MODULES = ['ipspiders.spiders']
+NEWSPIDER_MODULE = 'ipspiders.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'ipspiders (+http://www.yourdomain.com)'
